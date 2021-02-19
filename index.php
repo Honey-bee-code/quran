6e51175f74ad9a4e86c54c6afe1d577383138f4a
Honey-bee-code <hanifinida@gmail.com>
2021-02-19 13:28:15 +0800

    fix index

--- index.php
+++ index.php
@@ -3,11 +3,10 @@
 <div class="container">
     <div class="jumbotron">
     <h1 class="display-4">Assalamu 'alaikum, </h1>
-    <p class="lead">Selamat Datang di Aplikasi Qur'an Digital</p>
+    <p class="lead">Selamat Datang di Aplikasi Tafsir Qur'an Digital</p>
     <hr class="my-4">
     <p>Semoga bermanfaat di dunia dan akhirat.</p>
     <!-- <a class="btn btn-primary btn-lg" href="#" role="button">Pelajari selengkapnya</a> -->
-    </div>
     <form action="cari.php" class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-1" name="cari" type="search" placeholder="Cari" aria-label="Cari">
         <select class="form-control mr-sm-1" name="tafsir" id="">
@@ -18,6 +17,7 @@
         </select>
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Cari</button>
     </form>
+    </div>
 </div>
 
 
