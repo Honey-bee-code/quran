da1fa4f654d184751d5acbfa0e94421ce822d091
Honey-bee-code <hanifinida@gmail.com>
2021-02-21 22:48:53 +0800

    add mysqli_set_charset()

--- _config/config.php
+++ _config/config.php
@@ -4,6 +4,7 @@ $koneksi = mysqli_connect('localhost', 'root', '', 'quran');
 if(mysqli_connect_errno()){
     echo mysqli_connect_error();
 }
+mysqli_set_charset( $koneksi, 'utf8');//agar tulisan arab di website muncul
 
 function base_url($url = null) {
     // $base_url = "http://localhost/quran";
